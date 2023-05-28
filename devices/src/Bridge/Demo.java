6bb1fb932bb29553dd794bac0fb6bd3b8c370ae6
André Felipe <andrefelipealexandre96@gmail.com>
2023-05-28 15:43:00 -0300

    Add files via upload

---
+++ devices/src/Bridge/Demo.java
@@ -0,0 +1,26 @@
+package Bridge;
+
+import Bridge.devices.Device;
+import Bridge.devices.Radio;
+import Bridge.devices.Tv;
+import Bridge.remotes.AdvancedRemote;
+import Bridge.remotes.BasicRemote;
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
